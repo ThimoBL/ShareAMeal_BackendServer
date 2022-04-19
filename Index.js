3837ab10f67f617493a1998ebb0f2171977b799c
Thimo Luijsterburg <Thimo.Luijsterburg@rietveld.nl>
2022-04-19 10:57:16 +0200

    basic api server file added.

---
+++ Index.js
@@ -0,0 +1,21 @@
+let http = require('http');
+
+http.createServer(function (request, response) {
+    console.log('Er is een request.');
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    let exampleArray = ["item1", "item2"];
+    let exampleObj = {
+        item1: "item1Val",
+        item2: "item2Val"
+    };
+
+    let json = JSON.stringify({
+        anObj: exampleObj,
+        anArray: exampleArray,
+        another: "item"
+    })
+    response.end(json);
+}).listen(3500);
+
+console.log('De server luistert op port 3500');
