71a9196b504934e57d69a0d859fff4ba16c7aece
Thimo Luijsterburg <Thimo.Luijsterburg@rietveld.nl>
2022-05-10 18:24:37 +0200

    minor fixes

--- Database/dbconnection.js
+++ Database/dbconnection.js
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
     multipleStatements: true,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    user: process.env.DB_USERNAME,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
 })
