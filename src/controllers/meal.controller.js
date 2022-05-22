176420a970b9cf4bf820edaf8fca586903105c8e
Thimo Luijsterburg <Thimo.Luijsterburg@rietveld.nl>
2022-05-22 20:39:08 +0200

    meal bug possible fix

--- src/controllers/meal.controller.js
+++ src/controllers/meal.controller.js
@@ -28,6 +28,13 @@ let mealController = {
             return mealObject[key];
         });
 
+        if (req.body.datetime.includes("T") || req.body.datetime.includes("Z")) {
+            return res.status(400).json({
+                statusCode: 400,
+                results: `Datetime is not in the right format!`
+            })
+        }
+
         dbconnection.getConnection((err, connection) => {
             if (err) throw (err);
 
